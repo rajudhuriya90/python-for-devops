6315009c9f4c681dcee4476ad9bf6184f5bbea1e
rajudhuriya <raju24devops@gmail.com>
2026-03-09 21:50:29 +0530

    this program basic arithmatic calculation using variable

---
+++ python-basics/python-variables/variable_calculation.py
@@ -0,0 +1,14 @@
+# Variables for numbers
+
+a = 10
+b = 5
+
+sum_result = a + b
+sub_result = a - b
+mul_result = a * b
+div_result = a / b
+
+print("Addition:", sum_result)
+print("Subtraction:", sub_result)
+print("Multiplication:", mul_result)
+print("Division:", div_result)
