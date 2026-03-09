677cdb9d0a3c935d3e65d7e33c3516f83c215108
rajudhuriya <raju24devops@gmail.com>
2026-03-09 21:57:36 +0530

    this program show system info using variable

---
+++ python-basics/python-variables/multiplevariable.py
@@ -0,0 +1,9 @@
+# Variables representing system status
+
+cpu_usage = 65
+memory_usage = 70
+disk_space = 80
+
+print("CPU Usage:", cpu_usage, "%")
+print("Memory Usage:", memory_usage, "%")
+print("Disk Usage:", disk_space, "%")
