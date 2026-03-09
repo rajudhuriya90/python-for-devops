6026e41a30039c16f3a6195b68ace2b6ba3fdf58
rajudhuriya <raju24devops@gmail.com>
2026-03-09 21:46:37 +0530

    this program show info of the server

---
+++ python-basics/python-variables/server_info.py
@@ -0,0 +1,11 @@
+# Variables storing server details
+
+server_name = "web-server-01"
+ip_address = "192.168.1.10"
+operating_system = "Ubuntu"
+region = "Mumbai"
+
+print("Server Name:", server_name)
+print("IP Address:", ip_address)
+print("Operating System:", operating_system)
+print("Region:", region)
