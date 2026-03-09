30934177b70e23dc6f3cf076b40c5f444df7d6e8
rajudhuriya <raju24devops@gmail.com>
2026-03-09 21:30:33 +0530

    this is second variable program that stores student info

---
+++ python-basics/python-variables/studentsinfo.py
@@ -0,0 +1,11 @@
+# Variables storing student information
+
+name = "Raju"
+age = 22
+course = "DevOps"
+city = "Mumbai"
+
+print("Student Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("City:", city)
