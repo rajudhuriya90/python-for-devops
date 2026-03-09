5944fa66513c3a3f7fafe6e21f546409b63434e7
rajudhuriya <raju24devops@gmail.com>
2026-03-09 22:17:22 +0530

    Integer datatype that stored whole numbers values

---
+++ python-basics/python-datatypes/interger.py
@@ -0,0 +1,21 @@
+# Used to store whole numbers.
+
+age = 22
+year = 2026
+
+print(age)
+print(year)
+
+##SECOND EXAMPLE ###
+
+principal = 5000
+rate = 5
+time = 2
+
+interest = (principal * rate * time) // 100
+
+print("Principal:", principal)
+print("Rate:", rate)
+print("Time:", time)
+print("Simple Interest:", interest)
+
