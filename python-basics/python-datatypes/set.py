2731a020bfc9389b4e87ff5ff86e084980b7a22a
rajudhuriya <raju24devops@gmail.com>
2026-03-11 22:09:21 +0530

    this program show set datatype

--- python-basics/python-datatypes/set.py
+++ python-basics/python-datatypes/set.py
@@ -4,3 +4,25 @@ services = {"nginx", "docker", "ssh", "nginx"}
 print("Services:", services)
 
 print("Total Unique Services:", len(services))
+
+
+
+############## this program for the practice#####
+cpu_usage = 75
+memory_usage = 68
+disk_usage = 85
+
+average = (cpu_usage + memory_usage + disk_usage) // 3
+
+print("CPU Usage:", cpu_usage)
+print("Memory Usage:", memory_usage)
+print("Disk Usage:", disk_usage)
+
+print("Average Usage:", average)
+
+if average > 80:
+    print("Server Status: Critical")
+elif average > 60:
+    print("Server Status: Moderate Load")
+else:
+    print("Server Status: Healthy")
