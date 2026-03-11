21a27d536ea3863366890e5df77e20a036c17d05
rajudhuriya <raju24devops@gmail.com>
2026-03-11 22:18:56 +0530

    this all program based on if-statement

---
+++ python-basics/condional statements/if-statements.py
@@ -0,0 +1,32 @@
+##  Example 1 ##
+
+cpu = 85
+if cpu > 80:
+    print("High CPU usage")
+
+## Example 2 ##
+
+disk = 90
+if disk > 85:
+    print("Disk almost full")
+
+## Example 3 ##
+
+users = 120
+if users > 100:
+    print("High traffic on server")
+
+
+## Example 4 ##
+
+memory = 75
+if memory > 70:
+    print("Memory usage increasing")
+
+## Example 5 ##    
+
+containers = 15
+if containers > 10:
+    print("Many containers running")
+
+
