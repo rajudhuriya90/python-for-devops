f529861e0c1785ef30a6b24aa935ce9c236ce925
rajudhuriya <raju24devops@gmail.com>
2026-03-13 01:39:11 +0530

    this program represent pass-statements

---
+++ python-basics/looping-statements/pass-statements.py
@@ -0,0 +1,25 @@
+## Example 1
+for i in range(5):
+    if i == 2:
+        pass
+    print(i)
+
+
+## Example 2
+for service in ["nginx", "docker"]:
+    pass
+
+
+## Example 3
+if True:
+    pass
+
+## Example 4
+for i in range(3):
+    if i == 1:
+        pass
+    print("Number:", i)
+
+## Example 5
+while False:
+    pass
