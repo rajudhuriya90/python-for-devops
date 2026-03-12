71a46e19a09c5dc8bc6b26bdbbd64c5eb098a8b6
rajudhuriya <raju24devops@gmail.com>
2026-03-13 01:22:22 +0530

    this progream represent while-loop program

---
+++ python-basics/looping-statements/while-loop.py
@@ -0,0 +1,40 @@
+
+## Example 1
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+
+
+## Example 2
+count = 0
+
+while count < 3:
+    print("Hello DevOps")
+    count += 1
+
+
+## Example 3
+x = 1
+
+while x <= 4:
+    print("Loop:", x)
+    x += 1
+
+## Example 4
+num = 10
+
+while num > 5:
+    print(num)
+    num -= 1
+
+
+## Example 5
+
+a = 0
+
+while a < 5:
+    print("Value:", a)
+    a += 1
