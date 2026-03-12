e126536861702340b2551a9b8f825f7f83ccfb45
rajudhuriya <raju24devops@gmail.com>
2026-03-13 01:15:52 +0530

    this program represent for-loop-statements

---
+++ python-basics/looping-statements/for-loop.py
@@ -0,0 +1,24 @@
+## EXAMPLE 1
+for i in range(1, 6):
+    print(i)
+
+## EXAMPLE 2
+services = ["nginx", "docker", "ssh"]
+
+for service in services:
+    print(service)
+
+## EXAMPLE 3
+for i in range(0, 10, 2):
+    print(i)
+
+## EXAMPLE 4
+servers = ["server1", "server2", "server3"]
+
+for server in servers:
+    print("Checking", server)
+
+## EXAMPLE 5
+
+for i in range(5):
+    print("DevOps Practice")
